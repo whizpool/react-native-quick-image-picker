4d9a2e47ef7fa9d4b8b635029c9c76ea440abd86
Muhammad Kokab Waqas <kokabwaqas@gmail.com>
2022-01-04 14:19:07 +0500

    first version of library

---
+++ ios/ImagePickerManager.m
@@ -0,0 +1,478 @@
+#import "ImagePickerManager.h"
+#import "ImagePickerUtils.h"
+#import <React/RCTConvert.h>
+#import <AVFoundation/AVFoundation.h>
+#import <Photos/Photos.h>
+#import <PhotosUI/PhotosUI.h>
+
+@import MobileCoreServices;
+
+@interface ImagePickerManager ()
+
+@property (nonatomic, strong) RCTResponseSenderBlock callback;
+@property (nonatomic, copy) NSDictionary *options;
+
+@end
+
+@interface ImagePickerManager (UIImagePickerControllerDelegate) <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@end
+
+@interface ImagePickerManager (UIAdaptivePresentationControllerDelegate) <UIAdaptivePresentationControllerDelegate>
+@end
+
+#if __has_include(<PhotosUI/PHPicker.h>)
+@interface ImagePickerManager (PHPickerViewControllerDelegate) <PHPickerViewControllerDelegate>
+@end
+#endif
+
+@implementation ImagePickerManager
+
+NSString *errCameraUnavailable = @"camera_unavailable";
+NSString *errPermission = @"permission";
+NSString *errOthers = @"others";
+RNImagePickerTarget target;
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(launchCamera:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
+{
+    target = camera;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self launchImagePicker:options callback:callback];
+    });
+}
+
+RCT_EXPORT_METHOD(launchImageLibrary:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
+{
+    target = library;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self launchImagePicker:options callback:callback];
+    });
+}
+
+- (void)launchImagePicker:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback
+{
+    self.callback = callback;
+    
+    if (target == camera && [ImagePickerUtils isSimulator]) {
+        self.callback(@[@{@"errorCode": errCameraUnavailable}]);
+        return;
+    }
+    
+    self.options = options;
+
+#if __has_include(<PhotosUI/PHPicker.h>)
+    if (@available(iOS 14, *)) {
+        if (target == library) {
+            PHPickerConfiguration *configuration = [ImagePickerUtils makeConfigurationFromOptions:options target:target];
+            PHPickerViewController *picker = [[PHPickerViewController alloc] initWithConfiguration:configuration];
+            picker.delegate = self;
+            picker.presentationController.delegate = self;
+
+            if([self.options[@"includeExtra"] boolValue]) {
+                
+                [self checkPhotosPermissions:^(BOOL granted) {
+                    if (!granted) {
+                        self.callback(@[@{@"errorCode": errPermission}]);
+                        return;
+                    }
+                    [self showPickerViewController:picker];
+                }];
+            } else {
+                [self showPickerViewController:picker];
+            }
+            
+            return;
+        }
+    }
+#endif
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    [ImagePickerUtils setupPickerFromOptions:picker options:self.options target:target];
+    picker.delegate = self;
+    
+    if([self.options[@"includeExtra"] boolValue]) {
+        [self checkPhotosPermissions:^(BOOL granted) {
+            if (!granted) {
+                self.callback(@[@{@"errorCode": errPermission}]);
+                return;
+            }
+            [self showPickerViewController:picker];
+        }];
+    } else {
+      [self showPickerViewController:picker];
+    }
+}
+
+- (void) showPickerViewController:(UIViewController *)picker
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *root = RCTPresentedViewController();
+        [root presentViewController:picker animated:YES completion:nil];
+    });
+}
+
+#pragma mark - Helpers
+
+-(NSMutableDictionary *)mapImageToAsset:(UIImage *)image data:(NSData *)data phAsset:(PHAsset * _Nullable)phAsset {
+    NSString *fileType = [ImagePickerUtils getFileType:data];
+    
+    if ((target == camera) && [self.options[@"saveToPhotos"] boolValue]) {
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+    }
+    
+    if (![fileType isEqualToString:@"gif"]) {
+        image = [ImagePickerUtils resizeImage:image
+                                     maxWidth:[self.options[@"maxWidth"] floatValue]
+                                    maxHeight:[self.options[@"maxHeight"] floatValue]];
+    }
+
+    if ([fileType isEqualToString:@"jpg"]) {
+        data = UIImageJPEGRepresentation(image, [self.options[@"quality"] floatValue]);
+    } else if ([fileType isEqualToString:@"png"]) {
+        data = UIImagePNGRepresentation(image);
+    }
+    
+    NSMutableDictionary *asset = [[NSMutableDictionary alloc] init];
+    asset[@"type"] = [@"image/" stringByAppendingString:fileType];
+
+    NSString *fileName = [self getImageFileName:fileType];
+    NSString *path = [[NSTemporaryDirectory() stringByStandardizingPath] stringByAppendingPathComponent:fileName];
+    [data writeToFile:path atomically:YES];
+
+    if ([self.options[@"includeBase64"] boolValue]) {
+        asset[@"base64"] = [data base64EncodedStringWithOptions:0];
+    }
+
+    NSURL *fileURL = [NSURL fileURLWithPath:path];
+    asset[@"uri"] = [fileURL absoluteString];
+
+    NSNumber *fileSizeValue = nil;
+    NSError *fileSizeError = nil;
+    [fileURL getResourceValue:&fileSizeValue forKey:NSURLFileSizeKey error:&fileSizeError];
+    if (fileSizeValue){
+        asset[@"fileSize"] = fileSizeValue;
+    }
+
+    asset[@"fileName"] = fileName;
+    asset[@"name"] = [phAsset valueForKey:@"filename"];
+    asset[@"width"] = @(image.size.width);
+    asset[@"height"] = @(image.size.height);
+    
+    if(phAsset){
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        [formatter setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+        [formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSSZ"];
+        NSString *creationDate = [formatter stringFromDate:phAsset.creationDate];
+        
+        asset[@"timestamp"] = creationDate;
+        // Add more exif data here ...
+    }
+    
+    return asset;
+}
+
+-(NSMutableDictionary *)mapVideoToAsset:(NSURL *)url error:(NSError **)error {
+    NSString *fileName = [url lastPathComponent];
+    NSString *path = [[NSTemporaryDirectory() stringByStandardizingPath] stringByAppendingPathComponent:fileName];
+    NSURL *videoDestinationURL = [NSURL fileURLWithPath:path];
+
+    if ((target == camera) && [self.options[@"saveToPhotos"] boolValue]) {
+        UISaveVideoAtPathToSavedPhotosAlbum(url.path, nil, nil, nil);
+    }
+    
+    if (![url.URLByResolvingSymlinksInPath.path isEqualToString:videoDestinationURL.URLByResolvingSymlinksInPath.path]) {
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        
+        // Delete file if it already exists
+        if ([fileManager fileExistsAtPath:videoDestinationURL.path]) {
+            [fileManager removeItemAtURL:videoDestinationURL error:nil];
+        }
+
+        if (url) { // Protect against reported crash
+
+          // If we have write access to the source file, move it. Otherwise use copy.
+          if ([fileManager isWritableFileAtPath:[url path]]) {
+            [fileManager moveItemAtURL:url toURL:videoDestinationURL error:error];
+          } else {
+            [fileManager copyItemAtURL:url toURL:videoDestinationURL error:error];
+          }
+
+          if (error) {
+              return nil;
+          }
+        }
+    }
+    
+    NSMutableDictionary *asset = [[NSMutableDictionary alloc] init];
+    asset[@"fileName"] = fileName;
+    asset[@"duration"] = [NSNumber numberWithDouble:CMTimeGetSeconds([AVAsset assetWithURL:videoDestinationURL].duration)];
+    asset[@"uri"] = videoDestinationURL.absoluteString;
+    asset[@"type"] = [ImagePickerUtils getFileTypeFromUrl:videoDestinationURL];
+    asset[@"fileSize"] = [ImagePickerUtils getFileSizeFromUrl:videoDestinationURL];
+    
+    return asset;
+}
+
+- (void)checkCameraPermissions:(void(^)(BOOL granted))callback
+{
+    AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+    if (status == AVAuthorizationStatusAuthorized) {
+        callback(YES);
+        return;
+    }
+    else if (status == AVAuthorizationStatusNotDetermined){
+        [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+            callback(granted);
+            return;
+        }];
+    }
+    else {
+        callback(NO);
+    }
+}
+
+- (void)checkPhotosPermissions:(void(^)(BOOL granted))callback
+{
+    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
+    if (status == PHAuthorizationStatusAuthorized) {
+        callback(YES);
+        return;
+    } else if (status == PHAuthorizationStatusNotDetermined) {
+        [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+            if (status == PHAuthorizationStatusAuthorized) {
+                callback(YES);
+                return;
+            }
+            else {
+                callback(NO);
+                return;
+            }
+        }];
+    }
+    else {
+        callback(NO);
+    }
+}
+
+// Both camera and photo write permission is required to take picture/video and store it to public photos
+- (void)checkCameraAndPhotoPermission:(void(^)(BOOL granted))callback
+{
+    [self checkCameraPermissions:^(BOOL cameraGranted) {
+        if (!cameraGranted) {
+            callback(NO);
+            return;
+        }
+
+        [self checkPhotosPermissions:^(BOOL photoGranted) {
+            if (!photoGranted) {
+                callback(NO);
+                return;
+            }
+            callback(YES);
+        }];
+    }];
+}
+
+- (void)checkPermission:(void(^)(BOOL granted)) callback
+{
+    void (^permissionBlock)(BOOL) = ^(BOOL permissionGranted) {
+        if (!permissionGranted) {
+            callback(NO);
+            return;
+        }
+        callback(YES);
+    };
+
+    if (target == camera && [self.options[@"saveToPhotos"] boolValue]) {
+        [self checkCameraAndPhotoPermission:permissionBlock];
+    }
+    else if (target == camera) {
+        [self checkCameraPermissions:permissionBlock];
+    }
+    else {
+        if (@available(iOS 11.0, *)) {
+            callback(YES);
+        }
+        else {
+            [self checkPhotosPermissions:permissionBlock];
+        }
+    }
+}
+
+- (NSString *)getImageFileName:(NSString *)fileType
+{
+    NSString *fileName = [[NSUUID UUID] UUIDString];
+    fileName = [fileName stringByAppendingString:@"."];
+    return [fileName stringByAppendingString:fileType];
+}
+
++ (UIImage *)getUIImageFromInfo:(NSDictionary *)info
+{
+    UIImage *image = info[UIImagePickerControllerEditedImage];
+    if (!image) {
+        image = info[UIImagePickerControllerOriginalImage];
+    }
+    return image;
+}
+
++ (NSURL *)getNSURLFromInfo:(NSDictionary *)info {
+    if (@available(iOS 11.0, *)) {
+        return info[UIImagePickerControllerImageURL];
+    }
+    else {
+        return info[UIImagePickerControllerReferenceURL];
+    }
+}
+
+@end
+
+@implementation ImagePickerManager (UIImagePickerControllerDelegate)
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    dispatch_block_t dismissCompletionBlock = ^{
+        NSMutableArray<NSDictionary *> *assets = [[NSMutableArray alloc] initWithCapacity:1];
+
+        if ([info[UIImagePickerControllerMediaType] isEqualToString:(NSString *) kUTTypeImage]) {
+            PHAsset *asset = nil;
+            UIImage *image = [ImagePickerManager getUIImageFromInfo:info];
+            
+            // If include exif, we fetch the PHAsset, this required library permissions
+            if([self.options[@"includeExtra"] boolValue]) {
+                NSURL *referenceURL = [info objectForKey:UIImagePickerControllerReferenceURL];
+                
+                if(referenceURL != nil) {
+                    // We fetch the asset like this to support iOS 10 and lower
+                    // see: https://stackoverflow.com/a/52529904/4177049
+                    PHFetchResult* fetchResult = [PHAsset fetchAssetsWithALAssetURLs:@[referenceURL] options:nil];
+                    asset = fetchResult.firstObject;
+                }
+            }
+            
+            [assets addObject:[self mapImageToAsset:image data:[NSData dataWithContentsOfURL:[ImagePickerManager getNSURLFromInfo:info]] phAsset:asset]];
+        } else {
+            NSError *error;
+            NSDictionary *asset = [self mapVideoToAsset:info[UIImagePickerControllerMediaURL] error:&error];
+            if (asset == nil) {
+                self.callback(@[@{@"errorCode": errOthers, @"errorMessage":  error.localizedFailureReason}]);
+                return;
+            }
+            [assets addObject:asset];
+        }
+
+        NSMutableDictionary *response = [[NSMutableDictionary alloc] init];
+        response[@"assets"] = assets;
+        self.callback(@[response]);
+    };
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [picker dismissViewControllerAnimated:YES completion:dismissCompletionBlock];
+    });
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [picker dismissViewControllerAnimated:YES completion:^{
+            self.callback(@[@{@"didCancel": @YES}]);
+        }];
+    });
+}
+
+@end
+
+@implementation ImagePickerManager (presentationControllerDidDismiss)
+
+- (void)presentationControllerDidDismiss:(UIPresentationController *)presentationController
+{
+    self.callback(@[@{@"didCancel": @YES}]);
+}
+
+@end
+
+#if __has_include(<PhotosUI/PHPicker.h>)
+@implementation ImagePickerManager (PHPickerViewControllerDelegate)
+
+- (void)picker:(PHPickerViewController *)picker didFinishPicking:(NSArray<PHPickerResult *> *)results API_AVAILABLE(ios(14))
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
+    if (results.count == 0) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.callback(@[@{@"didCancel": @YES}]);
+        });
+        return;
+    }
+
+    dispatch_group_t completionGroup = dispatch_group_create();
+    NSMutableArray<NSDictionary *> *assets = [[NSMutableArray alloc] initWithCapacity:results.count];
+
+    for (PHPickerResult *result in results) {
+        PHAsset *asset = nil;
+        NSItemProvider *provider = result.itemProvider;
+
+        // If include exif, we fetch the PHAsset, this required library permissions
+//        if([self.options[@"includeExtra"] boolValue] && result.assetIdentifier != nil) {
+            PHFetchResult* fetchResult = [PHAsset fetchAssetsWithLocalIdentifiers:@[result.assetIdentifier] options:nil];
+            asset = fetchResult.firstObject;
+//        }
+        
+        dispatch_group_enter(completionGroup);
+
+        if ([provider canLoadObjectOfClass:[UIImage class]]) {
+            NSString *identifier = provider.registeredTypeIdentifiers.firstObject;
+            if ([identifier isEqualToString:@"com.apple.live-photo-bundle"]) {
+                // Handle live photos
+                identifier = @"public.jpeg";
+            }
+
+            [provider loadFileRepresentationForTypeIdentifier:identifier completionHandler:^(NSURL * _Nullable url, NSError * _Nullable error) {
+                NSData *data = [[NSData alloc] initWithContentsOfURL:url];
+                UIImage *image = [[UIImage alloc] initWithData:data];
+                
+                [assets addObject:[self mapImageToAsset:image data:data phAsset:asset]];
+                dispatch_group_leave(completionGroup);
+            }];
+        } else if ([provider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeMovie]) {
+//            [provider loadFileRepresentationForTypeIdentifier:(NSString *)kUTTypeMovie completionHandler:^(NSURL * _Nullable url, NSError * _Nullable error) {
+//                [assets addObject:[self mapVideoToAsset:url error:nil]];
+//                dispatch_group_leave(completionGroup);
+//            }];
+            PHVideoRequestOptions *videoRequestOptions = [[PHVideoRequestOptions alloc] init];
+            videoRequestOptions.version = PHVideoRequestOptionsVersionCurrent;
+            videoRequestOptions.deliveryMode = PHVideoRequestOptionsDeliveryModeAutomatic;
+            videoRequestOptions.networkAccessAllowed = YES;
+            
+            [[PHImageManager defaultManager] requestAVAssetForVideo:asset options:videoRequestOptions resultHandler:^(AVAsset *assetV, AVAudioMix *audioMix, NSDictionary *info) {
+                AVURLAsset *avssetVideo = (AVURLAsset *)assetV;
+                if (avssetVideo) {
+                        NSURL *originURL = [avssetVideo URL];
+                        // Now you have the URL of the original video.
+                    [assets addObject:[self mapVideoToAsset:originURL error:nil]];
+                    dispatch_group_leave(completionGroup);
+                }
+            }];
+        } else {
+            // The provider didn't have an item matching photo or video (fails on M1 Mac Simulator)
+            dispatch_group_leave(completionGroup);
+        }
+    }
+
+    dispatch_group_notify(completionGroup, dispatch_get_main_queue(), ^{
+        //  mapVideoToAsset can fail and return nil.
+        for (NSDictionary *asset in assets) {
+            if (nil == asset) {
+                self.callback(@[@{@"errorCode": errOthers}]);
+                return;
+            }
+        }
+
+        NSMutableDictionary *response = [[NSMutableDictionary alloc] init];
+        [response setObject:assets forKey:@"assets"];
+
+        self.callback(@[response]);
+    });
+}
+
+@end
+#endif
---
+++ ios/ImagePickerUtils.h
@@ -0,0 +1,21 @@
+#import "ImagePickerManager.h"
+
+@class PHPickerConfiguration;
+
+@interface ImagePickerUtils : NSObject
+
++ (BOOL)isSimulator;
+
++ (void)setupPickerFromOptions:(UIImagePickerController *)picker options:(NSDictionary *)options target:(RNImagePickerTarget)target;
+
++ (PHPickerConfiguration *)makeConfigurationFromOptions:(NSDictionary *)options target:(RNImagePickerTarget)target API_AVAILABLE(ios(14));
+
++ (NSString*)getFileType:(NSData*)imageData;
+
++ (UIImage*)resizeImage:(UIImage*)image maxWidth:(float)maxWidth maxHeight:(float)maxHeight;
+
++ (NSString *) getFileTypeFromUrl:(NSURL *)url;
+
++ (NSString *) getFileSizeFromUrl:(NSURL *)url;
+    
+@end
